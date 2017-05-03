b40e5e61cf66a1be72a6708afba2d5dd1ab7f808
Brian <broldak@gmail.com>
2017-05-02 21:49:46 -0700

    add plan model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503024405) do
+ActiveRecord::Schema.define(version: 20170503025603) do
+
+  create_table "plans", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
