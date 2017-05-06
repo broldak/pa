33300a42238e22eedfbc441dc2c9e995829d5414
Brian <broldak@gmail.com>
2017-05-06 15:42:18 -0700

    add plans model / controller / views

---
+++ app/controllers/plans_controller.rb
@@ -0,0 +1,54 @@
+class PlansController < ApplicationController
+  before_action :set_plan, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @plans = Plan.all
+  end
+
+  def show
+  end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+
+    respond_to do |format|
+      if @plan.save
+        format.html { redirect_to @plan, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @plan }
+      else
+        format.html { render :new }
+        format.json { render json: @plan.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @plan.update(plan_params)
+        format.html { redirect_to @plan, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @plan }
+      else
+        format.html { render :edit }
+        format.json { render json: @plan.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_plan
+      @plan = Plan.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def plan_params
+      params.require(:plan).permit(:title, :body_text, :user_id)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503025603) do
+ActiveRecord::Schema.define(version: 20170506223138) do
 
   create_table "plans", force: :cascade do |t|
     t.string   "title"
     t.string   "body_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
