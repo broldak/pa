5106bb8de2e685cdbb762d8223029004e7b29aeb
Brian <broldak@gmail.com>
2017-05-08 23:57:50 -0700

    add a bunch of styles / markup

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post '/login',    to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :users
+  resources :users, :except => [:index]
 
   resources :plans
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
